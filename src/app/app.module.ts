05f9e9c2a5d6774eea9468149f5487c6836b5e3e
adrmenam <alex_m6m@hotmail.com>
2019-05-03 18:16:42 -0500

    NF_Component creation_01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { CreateAppointmentComponent } from './create-appointment/create-appointment.component';
+import { EditAppointmentComponent } from './edit-appointment/edit-appointment.component';
+import { PatientsComponent } from './patients/patients.component';
+import { CreatePatientComponent } from './create-patient/create-patient.component';
+import { EditPatientComponent } from './edit-patient/edit-patient.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentsComponent,
+    CreateAppointmentComponent,
+    EditAppointmentComponent,
+    PatientsComponent,
+    CreatePatientComponent,
+    EditPatientComponent
   ],
   imports: [
     BrowserModule,
